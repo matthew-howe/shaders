1f18d2dbc42138b2b49fdc72b5417feaf9a89f77
matthew-howe <mattehowe@gmail.com>
2020-01-20 20:03:07 -0500

    fixes library origin error

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const path = require('path');
 const app = express();
 const serveIndex = require('serve-index');
 
+
+
 app.use('/', express.static(path.join(__dirname, 'shaders')), serveIndex(path.join(__dirname, 'shaders'), {'icons': true}));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
